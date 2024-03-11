cf3679018be3f90db0f2f1e9433a966692976421
Green Sky <green@g-s.xyz>
2024-03-11 11:45:03 +0100

    toxcore update and add private ngc ip getter

--- solanaceae/toxcore/tox_private_interface.hpp
+++ solanaceae/toxcore/tox_private_interface.hpp
@@ -2,13 +2,21 @@
 
 #include <cstdint>
 
+#include <string>
+#include <optional>
+#include <tuple>
+
+#include "./toxcore_enums.hpp"
+
 // defines the private interface for tox
 struct ToxPrivateI {
-	static constexpr const char* version {"1"};
+	static constexpr const char* version {"2"};
 
 	virtual ~ToxPrivateI(void) {}
 
 	virtual uint16_t toxDHTGetNumCloselist(void) = 0;
 	virtual uint16_t toxDHTGetNumCloselistAnnounceCapable(void) = 0;
+
+	virtual std::tuple<std::optional<std::string>, Tox_Err_Group_Peer_Query> toxGroupPeerGetIPAddress(uint32_t group_number, uint32_t peer_id) = 0;
 };
 
